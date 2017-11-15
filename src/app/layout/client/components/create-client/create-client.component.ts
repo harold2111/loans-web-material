4013202cf6684d220f308678b30dc571691ac788
hmerino <harold2111@gmail.com>
2017-11-14 21:16:20 -0500

    inital template with routing

---
+++ src/app/layout/client/components/create-client/create-client.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-create-client',
+  templateUrl: './create-client.component.html',
+  styleUrls: ['./create-client.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CreateClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
