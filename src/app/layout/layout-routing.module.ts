93c3d488944a429cef733839a2c2cee8ec1f83c9
hmerino <harold2111@gmail.com>
2017-11-15 21:51:56 -0500

    working in layout

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -3,19 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: LayoutComponent,
-        children: [
-            { path: '', redirectTo: 'client' },
-            { path: 'dashboard', loadChildren: './blank-page/blank-page.module#BlankPageModule'},
-            { path: 'client', loadChildren: './client/client.module#ClientModule' }
-        ]
-    }
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'client' },
+      { path: 'dashboard', loadChildren: './blank-page/blank-page.module#BlankPageModule' },
+      { path: 'client', loadChildren: './client/client.module#ClientModule' }
+    ]
+  }
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
-export class LayoutRoutingModule {}
+export class LayoutRoutingModule { }
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,10 +4,14 @@ import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 
+// Material Design Component
+import {MaterialModule} from '../material.module'
+
 @NgModule({
   imports: [
     CommonModule,    
-    LayoutRoutingModule
+    LayoutRoutingModule,
+    MaterialModule
   ],
   declarations: [LayoutComponent]
 })
